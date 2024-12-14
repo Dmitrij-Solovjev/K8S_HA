ec7ee241e319c8efc516470170157f03d1fab57e
Dmitrij <dima_solovjev03@mail.ru>
2024-12-14 15:20:34 +0300

    modified:   Start_rabbitMQ.sh

--- Start_rabbitMQ.sh
+++ Start_rabbitMQ.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 ###Install the RabbitMQ operator 
-#kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml
-#sleep 1
+kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml
+sleep 1
 
 ###Check if the components are healthy in the rabbitmq-system namespace
 #kubectl get all -o wide -n rabbitmq-system
 
-#kubectl apply -f rabbitmqcluster.yaml
+kubectl apply -f rabbitmqcluster.yaml
 #sleep 1
 
 # Последний тест
 #kubectl delete all --all
 
-kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml
-sleep 1
-kubectl apply -f https://raw.githubusercontent.com/rabbitmq/cluster-operator/main/docs/examples/hello-world/rabbitmq.yaml
-sleep 1
+#kubectl apply -f https://github.com/rabbitmq/cluster-operator/releases/latest/download/cluster-operator.yml
+#sleep 1
+#kubectl apply -f https://raw.githubusercontent.com/rabbitmq/cluster-operator/main/docs/examples/hello-world/rabbitmq.yaml
+#sleep 1
 # Согласно документации должно работать, хоть и нет портов наружу как в верхнем.
 # Для получения доступа к админке используйте
 #username="$(kubectl get secret hello-world-default-user -o jsonpath='{.data.username}' | base64 --decode)"
